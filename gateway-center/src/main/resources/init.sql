cc1541c3aac92a5bc257a759ae0654c2667246c1
qingshi <705029004@qq.com>
2024-07-18 22:55:26 +0800

    sqlite ddd

--- gateway-center/src/main/resources/init.sql
+++ gateway-center/src/main/resources/init.sql
@@ -1,32 +1,34 @@
 
+DROP TABLE IF EXISTS http_statement;
+CREATE TABLE http_statement (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                application TEXT NOT NULL COMMENT '应用名称',
+                                interface_name TEXT NOT NULL COMMENT '服务接口；RPC、其他',
+                                method_name TEXT NOT NULL COMMENT '服务方法；RPC#method',
+                                parameter_type TEXT NOT NULL COMMENT '参数类型(RPC 限定单参数注册)；new String[]{"java.lang.String"}、new String[]{"cn.bugstack.gateway.rpc.dto.XReq"}',
+                                uri TEXT NOT NULL COMMENT '网关接口',
+                                http_command_type TEXT NOT NULL COMMENT '接口类型；GET、POST、PUT、DELETE',
+                                auth INTEGER NOT NULL DEFAULT 0 COMMENT 'true = 1是、false = 0否',
+                                create_time TEXT, -- 你可以在这里使用DATETIME或TEXT，取决于你的需求
+                                update_time TEXT, -- 同样，你可以使用DATETIME或TEXT
+    -- 你可能需要创建一个触发器来更新update_time字段
+    -- 触发器示例将在下面给出
+    -- ...
+);
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for http_statement
--- ----------------------------
-DROP TABLE IF EXISTS `http_statement`;
-CREATE TABLE `http_statement` (
-  `id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `application` varchar(128) COLLATE utf8_bin NOT NULL COMMENT '应用名称',
-  `interface_name` varchar(256) COLLATE utf8_bin NOT NULL COMMENT '服务接口；RPC、其他',
-  `method_name` varchar(128) COLLATE utf8_bin NOT NULL COMMENT ' 服务方法；RPC#method',
-  `parameter_type` varchar(256) COLLATE utf8_bin NOT NULL COMMENT '参数类型(RPC 限定单参数注册)；new String[]{"java.lang.String"}、new String[]{"cn.bugstack.gateway.rpc.dto.XReq"}',
-  `uri` varchar(128) COLLATE utf8_bin NOT NULL COMMENT '网关接口',
-  `http_command_type` varchar(32) COLLATE utf8_bin NOT NULL COMMENT '接口类型；GET、POST、PUT、DELETE',
-  `auth` int(4) NOT NULL DEFAULT '0' COMMENT 'true = 1是、false = 0否',
-  `create_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- ----------------------------
--- Records of http_statement
--- ----------------------------
-BEGIN;
-INSERT INTO `http_statement` VALUES (1, 'api-gateway-test', 'io.github.chenyilei2016.providerApi.IActivityBooth', 'sayHi', 'java.lang.String', '/wg/activity/sayHi', 'GET', 0, '2022-10-22 15:30:00', '2022-10-22 15:30:00');
-INSERT INTO `http_statement` VALUES (2, 'api-gateway-test', 'io.github.chenyilei2016.providerApi.IActivityBooth', 'insert', 'io.github.chenyilei2016.providerApi.dto.XReq', '/wg/activity/insert', 'POST', 1, '2022-10-22 15:30:00', '2022-10-22 15:30:00');
+-- 插入数据的SQL保持不变（因为数据格式是兼容的）
+BEGIN TRANSACTION;
+INSERT INTO http_statement (application, interface_name, method_name, parameter_type, uri, http_command_type, auth, create_time, update_time) VALUES
+    ('api-gateway-test', 'io.github.chenyilei2016.providerApi.IActivityBooth', 'sayHi', 'java.lang.String', '/wg/activity/sayHi', 'GET', 0, '2022-10-22 15:30:00', '2022-10-22 15:30:00');
+INSERT INTO http_statement (application, interface_name, method_name, parameter_type, uri, http_command_type, auth, create_time, update_time) VALUES
+    ('api-gateway-test', 'io.github.chenyilei2016.providerApi.IActivityBooth', 'insert', 'io.github.chenyilei2016.providerApi.dto.XReq', '/wg/activity/insert', 'POST', 1, '2022-10-22 15:30:00', '2022-10-22 15:30:00');
 COMMIT;
 
-SET FOREIGN_KEY_CHECKS = 1;
+-- 触发器示例来更新update_time字段（当其他字段更新时）
+CREATE TRIGGER update_http_statement_update_time
+    AFTER UPDATE ON http_statement
+    FOR EACH ROW
+    WHEN OLD.id IS NOT NULL
+BEGIN
+    UPDATE http_statement SET update_time = datetime('now') WHERE id = OLD.id;
+END;
