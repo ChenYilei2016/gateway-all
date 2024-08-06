eede8e5f933308d1c8d2e8d75836e34c4af67a7c
陈忆磊 <705029004@qq.com>
2024-08-06 20:18:53 +0800

    gateway center assist 13 ing

--- gateway-center/src/test/java/io/github/chenyilei2016/gateway/center/test/ApiGatewayApplicationTest.java
+++ gateway-center/src/test/java/io/github/chenyilei2016/gateway/center/test/ApiGatewayApplicationTest.java
@@ -3,7 +3,9 @@ package io.github.chenyilei2016.gateway.center.test;
 import com.alibaba.fastjson.JSON;
 import io.github.chenyilei2016.gateway.center.ApiGatewayApplication;
 import io.github.chenyilei2016.gateway.center.application.IConfigManageService;
+import io.github.chenyilei2016.gateway.center.application.IRegisterManageService;
 import io.github.chenyilei2016.gateway.center.domain.manage.model.vo.GatewayServerVO;
+import io.github.chenyilei2016.gateway.center.domain.register.model.vo.ApplicationInterfaceMethodVO;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -29,6 +31,8 @@ class ApiGatewayApplicationTest {
 
     @Resource
     private IConfigManageService configManageService;
+    @Resource
+    private IRegisterManageService registerManageService;
 
     @Test
     public void test_queryGatewayServerList() {
@@ -41,5 +45,30 @@ class ApiGatewayApplicationTest {
         configManageService.registerGatewayServerNode("10001", "api-gateway-g1", "电商支付网关", "127.0.0.196");
         configManageService.registerGatewayServerNode("10001", "api-gateway-g2", "电商支付网关", "127.0.0.197");
         configManageService.registerGatewayServerNode("10001", "api-gateway-g3", "电商配送网关", "127.0.0.198");
+    }
+
+    @Test
+    public void test_registerApplicationInterfaceMethod() {
+        ApplicationInterfaceMethodVO applicationInterfaceVO01 = new ApplicationInterfaceMethodVO();
+        applicationInterfaceVO01.setSystemId("api-gateway-test");
+        applicationInterfaceVO01.setInterfaceId("io.github.chenyilei2016.providerApi.IActivityBooth");
+        applicationInterfaceVO01.setMethodId("sayHi");
+        applicationInterfaceVO01.setMethodName("测试方法");
+        applicationInterfaceVO01.setParameterType("java.lang.String");
+        applicationInterfaceVO01.setUri("/wg/activity/sayHi");
+        applicationInterfaceVO01.setHttpCommandType("GET");
+        applicationInterfaceVO01.setAuth(0);
+        registerManageService.registerApplicationInterfaceMethod(applicationInterfaceVO01);
+
+        ApplicationInterfaceMethodVO applicationInterfaceVO02 = new ApplicationInterfaceMethodVO();
+        applicationInterfaceVO02.setSystemId("api-gateway-test");
+        applicationInterfaceVO02.setInterfaceId("io.github.chenyilei2016.providerApi.IActivityBooth");
+        applicationInterfaceVO02.setMethodId("insert");
+        applicationInterfaceVO02.setMethodName("插入方法");
+        applicationInterfaceVO02.setParameterType("io.github.chenyilei2016.providerApi.dto.XReq");
+        applicationInterfaceVO02.setUri("/wg/activity/insert");
+        applicationInterfaceVO02.setHttpCommandType("POST");
+        applicationInterfaceVO02.setAuth(1);
+        registerManageService.registerApplicationInterfaceMethod(applicationInterfaceVO02);
     }
 }
