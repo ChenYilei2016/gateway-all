7ad49d6eaac95dd1c4d8199bfad5300b284b054d
陈忆磊 <705029004@qq.com>
2024-08-08 20:58:03 +0800

    gateway center assist 13 ing

---
+++ gateway-assist/src/main/java/io/github/chenyilei2016/Main.java
@@ -0,0 +1,11 @@
+package io.github.chenyilei2016;
+
+/**
+ * @author chenyilei
+ * @since 2024/08/06 20:15
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
